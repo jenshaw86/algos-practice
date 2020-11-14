82779f663f0c2fa944dab526ef8325cb60c819c4
Jenny Shaw <bloo.j.shaw@gmail.com>
2020-11-14 13:50:22 -0800

    Export ListNode and LinkedList classes and create method

--- linked-list-implementation/linked-list-implementation.js
+++ linked-list-implementation/linked-list-implementation.js
@@ -11,7 +11,7 @@ class LinkedList {
     }
 }
 
-const createLinkedList = values => {
+const create = values => {
     let nodes = values.map(val => new ListNode(val));
 
     nodes.forEach((node, i) => node.next = nodes[i + 1]);
@@ -19,7 +19,13 @@ const createLinkedList = values => {
     return new LinkedList(nodes[0]);
 }
 
-const values = [1,2,3,4,5];
-let linkedList = createLinkedList(values);
+// let values = [1,2,3,4,5];
+// let linkedList = create(values);
 
-console.log(linkedList);
+// console.log(linkedList);
+
+module.exports = {
+    ListNode: ListNode,
+    LinkedList: LinkedList,
+    create: create
+}
