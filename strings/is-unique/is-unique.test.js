7cba20294a3ea5892814e5c84cdc021adf4f3658
Jenny Shaw <bloo.j.shaw@gmail.com>
2020-11-16 19:36:44 -0800

    Add new test to isUnique

--- strings/is-unique/is-unique.test.js
+++ strings/is-unique/is-unique.test.js
@@ -1,19 +1,24 @@
 const isUnique = require('./is-unique')
 
 describe('isUnique', () => {
-    it('returns true if string has no repeating characters', () => {
+    it('returns true if string 1 is empty', () => {
+        const str = '';
+        expect(isUnique(str)).toBe(false);
+    });
+
+    it('returns true if string 2 has no repeating characters', () => {
         const str = 'abcdefg';
         expect(isUnique(str)).toBe(true);
     });
     
-    it('returns true if string is empty', () => {
-        const str = '';
-        expect(isUnique(str)).toBe(false);
-    });
-    
+    it('returns true if string 3 has same characters with different cases', () => {
+        const str = 'ABba';
+        expect(isUnique(str)).toBe(true);
+    })
     it('returns false if string has any repeating characters', () => {
         const str = 'pizza';
         expect(isUnique(str)).toBe(false);
     });
+
 
 });
